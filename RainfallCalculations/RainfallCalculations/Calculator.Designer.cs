b8c84adb0954652ab22d3409994d62a563006adf
Babatope Ayeni <133984506+babatopeayeni@users.noreply.github.com>
2023-10-19 14:57:36 -0400

    Add files via upload

---
+++ RainfallCalculations/RainfallCalculations/Calculator.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace RainfallCalculations
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMaxRain = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalRecords = new System.Windows.Forms.Label();
+            this.lblbadrecord = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMinRain = new System.Windows.Forms.Label();
+            this.lbi = new System.Windows.Forms.Label();
+            this.frmFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusmessage = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusmessage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAverage);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblMaxRain);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblTotalRecords);
+            this.groupBox1.Controls.Add(this.lblbadrecord);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblMinRain);
+            this.groupBox1.Controls.Add(this.lbi);
+            this.groupBox1.Location = new System.Drawing.Point(233, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(430, 429);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result For Uploaded File";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(146, 211);
+            this.lblAverage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 13);
+            this.lblAverage.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(4, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ave. Rainfall\r\n Recorded";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblMaxRain
+            // 
+            this.lblMaxRain.AutoSize = true;
+            this.lblMaxRain.Location = new System.Drawing.Point(152, 170);
+            this.lblMaxRain.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMaxRain.Name = "lblMaxRain";
+            this.lblMaxRain.Size = new System.Drawing.Size(0, 13);
+            this.lblMaxRain.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(-3, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Highest Rainfall Recorded";
+            // 
+            // lblTotalRecords
+            // 
+            this.lblTotalRecords.AutoSize = true;
+            this.lblTotalRecords.Location = new System.Drawing.Point(147, 41);
+            this.lblTotalRecords.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalRecords.Name = "lblTotalRecords";
+            this.lblTotalRecords.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalRecords.TabIndex = 6;
+            // 
+            // lblbadrecord
+            // 
+            this.lblbadrecord.AutoSize = true;
+            this.lblbadrecord.Location = new System.Drawing.Point(147, 89);
+            this.lblbadrecord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblbadrecord.Name = "lblbadrecord";
+            this.lblbadrecord.Size = new System.Drawing.Size(0, 13);
+            this.lblbadrecord.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(-2, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Bad Records";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(0, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Minimum Rainfall \r\nRecorded";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblMinRain
+            // 
+            this.lblMinRain.AutoSize = true;
+            this.lblMinRain.Location = new System.Drawing.Point(149, 124);
+            this.lblMinRain.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMinRain.Name = "lblMinRain";
+            this.lblMinRain.Size = new System.Drawing.Size(0, 13);
+            this.lblMinRain.TabIndex = 1;
+            // 
+            // lbi
+            // 
+            this.lbi.AutoSize = true;
+            this.lbi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbi.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbi.Location = new System.Drawing.Point(0, 41);
+            this.lbi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbi.Name = "lbi";
+            this.lbi.Size = new System.Drawing.Size(134, 13);
+            this.lbi.TabIndex = 0;
+            this.lbi.Text = "Total Rainfall Records";
+            this.lbi.Click += new System.EventHandler(this.lbi_Click);
+            // 
+            // frmFileDialog
+            // 
+            this.frmFileDialog.FileName = "frmFileDialog";
+            this.frmFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.frmFileDialog_FileOk);
+            // 
+            // btnFile
+            // 
+            this.btnFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile.ForeColor = System.Drawing.Color.Chocolate;
+            this.btnFile.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFile.Location = new System.Drawing.Point(12, 100);
+            this.btnFile.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(185, 85);
+            this.btnFile.TabIndex = 3;
+            this.btnFile.Text = "Click To Select Rain Data File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(665, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusmessage
+            // 
+            this.statusmessage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.statusmessage.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusmessage.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusmessage.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusmessage.Location = new System.Drawing.Point(0, 429);
+            this.statusmessage.Name = "statusmessage";
+            this.statusmessage.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusmessage.Size = new System.Drawing.Size(665, 22);
+            this.statusmessage.TabIndex = 5;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.AutoSize = true;
+            this.lblmessage.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmessage.Location = new System.Drawing.Point(9, 437);
+            this.lblmessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(0, 13);
+            this.lblmessage.TabIndex = 6;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 451);
+            this.Controls.Add(this.lblmessage);
+            this.Controls.Add(this.statusmessage);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Calculator";
+            this.ShowIcon = false;
+            this.Text = "Rainfall Calculator";
+            this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusmessage.ResumeLayout(false);
+            this.statusmessage.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog frmFileDialog;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusmessage;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label lblMaxRain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalRecords;
+        private System.Windows.Forms.Label lblbadrecord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMinRain;
+        private System.Windows.Forms.Label lbi;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Label lblmessage;
+    }
+}
+
---
+++ RainfallCalculations/RainfallCalculations/Calculator.cs
@@ -0,0 +1,163 @@
+﻿//Name: Babatope Ayeni
+//Date: 12/14/2021
+//Exam: C# Exam – Rainfall Calculator
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RainfallCalculations
+{
+    public partial class Calculator : Form
+    {
+
+        private static string logFilePath = @"C:\temp\rainfall2.txt";
+
+        List<double> Record = new List<double>();
+        List<double> bRecord = new List<double>();
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = frmFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string file = frmFileDialog.FileName;
+                try
+                {
+                    double rainfalreading;
+
+                    int totalRecordCount = File.ReadAllLines(file).Length;
+
+                    int totalBadRecords = (from line in File.ReadAllLines(file)
+                                           where Double.TryParse(line, out rainfalreading) == false
+                                           select line).Count();
+
+                    List<string> list = (from line in File.ReadAllLines(file)
+                                         where Double.TryParse(line, out rainfalreading) == true
+                                         select line).ToList();
+
+                    foreach (var line in list)
+                    {
+                        // Do something with the line.
+                        if (Double.TryParse(line, out rainfalreading))
+                        {
+                            if (rainfalreading>=0)
+                            {
+                                Record.Add(rainfalreading);
+                            }
+                            else
+                            {
+                                bRecord.Add(rainfalreading);
+                            }
+                        }
+                    }
+
+                    lblMaxRain.Text = Record.Max().ToString();
+
+                    lblTotalRecords.Text = totalRecordCount.ToString();
+
+                    lblbadrecord.Text = bRecord.Count.ToString();
+
+                    lblMinRain.Text = Record.Min().ToString();
+
+                    lblAverage.Text= Record.Average().ToString();
+
+                    LogFileWrite(Record);
+                }
+                catch (System.IO.IOException ex)
+                {
+                    lblmessage.Text = "File Selected Has Error(" + ex.Message + ")";
+                }
+            }
+        }
+
+        public  void LogFileWrite(List<double> message)
+        {
+            FileStream fileStream = null;
+            StreamWriter streamWriter = null;
+            try
+            {
+                if (logFilePath.Equals("")) return;
+                #region Create the Log file directory if it does not exists
+
+                DirectoryInfo logDirInfo = null;
+
+                FileInfo logFileInfo = new FileInfo(logFilePath);
+
+                logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                #endregion Create the Log file directory if it does not exists
+
+                if (!logFileInfo.Exists)//check if the file exist
+                {
+                    fileStream = logFileInfo.Create();
+
+                    lblmessage.Text = "File Name:rainfall2.txt Does not Exist in (" + logFilePath + ") thus will be created";
+                }
+                else
+                {
+                    File.Delete(logFilePath);
+
+                    fileStream = new FileStream(logFilePath, FileMode.Append);
+
+                    lblmessage.Text = "File Name:rainfall2.txt Exist in the path(" + logFilePath + ")";
+                }
+                streamWriter = new StreamWriter(fileStream);
+                //streamWriter.WriteLine(message);
+                foreach (var s in message.OrderBy(p => p))
+                    streamWriter.WriteLine(s);
+            }
+            finally
+            {
+                if (streamWriter != null) streamWriter.Close();
+                if (fileStream != null) fileStream.Close();
+            }
+
+        }
+
+       private void lbi_Click(object sender, EventArgs e)
+        {
+
+       }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
